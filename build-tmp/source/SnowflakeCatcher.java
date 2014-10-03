931e0c9c20776c17946bd24927dd7b68d9b7d079
gigibyte327 <gigi.lu27@gmail.com>
2014-10-03 11:43:09 -0700

    added snowflake

--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SnowflakeCatcher extends PApplet {
+
 SnowFlake [] snows;
-void setup()
+public void setup()
 {
   size(300,300);
   background(0);
@@ -8,7 +24,7 @@ void setup()
     snows[i] = new SnowFlake();
   }
 }
-void draw()
+public void draw()
 {
   for (int i = 0; i<snows.length; i++){
     snows[i].erase();
@@ -18,7 +34,7 @@ void draw()
     snows[i].show(); 
   }
 }
-void mouseDragged()
+public void mouseDragged()
 {
 
   if(mouseButton != RIGHT){
@@ -45,15 +61,14 @@ class SnowFlake
     y=(int)(Math.random()*300);
     isMoving=true;
   }
-  void show()
+  public void show()
   {
     noStroke();
     fill(255);
     ellipse(x,y,5,5);
   }
-  void lookDown()
+  public void lookDown()
   {
-    //change get to lower number to slow down. like barrier
     if(y>=0 && y<=300){
       if(get(x,y+4)!=color(0,0,0)){
         isMoving=false;
@@ -63,19 +78,19 @@ class SnowFlake
       }
     }
   }
-  void erase()
+  public void erase()
   {
     noStroke();
     fill(0);
     ellipse(x,y,8,8);
   }
-  void move()
+  public void move()
   {
     if (isMoving==true){
       y+=1;
     }
   }
-  void wrap()
+  public void wrap()
   {
     if(y>=293){
       y=0;
@@ -83,4 +98,13 @@ class SnowFlake
     }
   }
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SnowflakeCatcher" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
